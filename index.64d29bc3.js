d00108ba21fee08e020b354a070bfef8f1945fbb
001elijah <job.illia.igorevich@gmail.com>
2023-01-31 15:57:10 +0000

    Deploying to gh-pages from @ 001elijah/bc-48-html-project-3@7acadb07b7d32046dab70955d02f0e6321df4620 🚀

--- index.64d29bc3.js
+++ index.64d29bc3.js
@@ -1,2 +1,2 @@
 !function(){var e={openMenuBtn:document.querySelector("[data-menu-open]"),closeMenuBtn:document.querySelector("[data-menu-close]"),menu:document.querySelector("[data-menu]"),body:document.querySelector("body"),menuList:document.querySelector(".mob-menu__list")};function n(){e.menu.classList.toggle("is-hidden"),e.body.classList.toggle("no-scroll")}e.openMenuBtn.addEventListener("click",n),e.closeMenuBtn.addEventListener("click",n),e.menuList.addEventListener("click",(function(){e.menu.classList.add("is-hidden"),e.body.classList.remove("no-scroll")}))}();
-//# sourceMappingURL=index.8a92d3af.js.map
+//# sourceMappingURL=index.64d29bc3.js.map
