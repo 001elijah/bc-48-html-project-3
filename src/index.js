f1bdf666ef65c54ade174c9e6ca46d3a66a7d7e0
DVorortnoy <dimchik505@gmail.com>
2023-01-31 20:42:50 +0200

    change js and some css

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
-import {funcModal} from "./js/modal.js";
-import {funcSlider} from "./js/slider.js";
-import{funcMenu} from "./js/menu.js";
+import {funcModal} from "./js/modal";
+// import {funcSlider} from "./js/slider.js";
+import{funcMenu} from "./js/menu";
 
 funcModal();
-funcSlider();
+// funcSlider();
 funcMenu();
